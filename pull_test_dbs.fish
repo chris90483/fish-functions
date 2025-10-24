4a86bc8ec1febc8b66791cf08beb48b20f56f2ab
Chris Jansen <c.jansen@oegematransport.nl>
2025-10-24 13:39:33 +0200

    remove non-generic functions

--- pull_test_dbs.fish
+++
@@ -1,3 +0,0 @@
-function pull_test_dbs
-  /home/chrisjansen/repos/chris-jansens-dev-dingetjes/pull_test_dbs/pull_test_dbs.sh "$argv"
-end
--- start_apis.fish
+++
@@ -1,3 +0,0 @@
-function start_apis
-  /home/chrisjansen/repos/chris-jansens-dev-dingetjes/start_apis/start_apis.sh "$argv"
-end
