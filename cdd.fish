c550bd84df6710a5ab8cfea02c2a2b5c622231fc
Chris Jansen <chris9898@live.nl>
2026-02-21 22:50:02 +0100

    'master: cdd: functie om te cd'en naar een drive'

---
+++ cdd.fish
@@ -0,0 +1,35 @@
+# (cd) naar een (d)rive
+function cdd
+	set -l lines (lsblk -nrpo LABEL,NAME,SIZE,MOUNTPOINT)
+	for line in $lines;
+		set -l parts (string split ' ' $line)
+		set -l pc (count $parts)
+
+		set label      $parts[1]
+		set name       $parts[2]
+		set size       $parts[3]		
+		set mountpoint $parts[4]
+
+		if test -n "$mountpoint"
+		    if test -z "$label"
+		        continue
+		    end
+
+		    set entries $entries "$label !| $name !| $size !| $mountpoint"
+		end
+	end
+	
+	if test (count $argv) -ge 1
+		set selected (printf "%s\n" $entries | column -t -s '!' | fzf --prompt="Selecteer drive: " --query="$argv[1]" --select-1)
+	else
+		set selected (printf "%s\n" $entries | column -t -s '!' | fzf --prompt="Selecteer drive: ")
+	end
+	
+	if test -n "$selected"
+		set -l parts (string split '|' $selected)
+		set -l label (echo "$parts[1]" | string trim)
+		set -l mountpoint (echo "$parts[4]" | string trim)
+		echo "cd naar $label op $mountpoint"
+		cd $mountpoint
+	end 
+end
