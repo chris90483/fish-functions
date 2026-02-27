22367db413dd6aa8c8ea3c05077a0044313f961f
Chris Jansen <c.jansen@oegematransport.nl>
2026-02-27 16:21:39 +0100

    'master: make nav separator gray'

--- nav.fish
+++ nav.fish
@@ -1,8 +1,9 @@
 # cd met pwd ls in de nieuwe directory
 function nav
-        set -l BLUE '\033[0;34m'
-        set -l NC '\033[0m' # No Color
-        set -l CYAN '\033[0;36m'
+    set -l GRAY '\033[0;90m'
+    set -l BLUE '\033[0;34m'
+    set -l NC '\033[0m' # No Color
+    set -l CYAN '\033[0;36m'
 
 	if test (count $argv) -gt 0
         	if not cd $argv
@@ -10,26 +11,28 @@ function nav
     	    end
 	end
 	echo ""
-	
-    	set rootdev (df -P / | tail -1 | awk '{print $1}')
-    	set curdev  (df -P $PWD | tail -1 | awk '{print $1}')
-    	
-    	######################
-    	# wd
-    	if test "$curdev" = "$rootdev"
-    		pwd
-    	else
-		set -l device (df -P $PWD | tail -1 | awk '{print $1}')
-		set -l label (lsblk -no LABEL $device 2>/dev/null)
-		set -l mountpoint (lsblk -no MOUNTPOINT $device 2>/dev/null)
-		set -l drive_wd (string replace -r "^$mountpoint" "" (pwd))
 
-		echo -e "$CYAN\133$label\135$NC$drive_wd"
-    	end
-    	
-    	######################
-	# seperator 
-	printf '%*s' "$(tput cols)" '' | tr ' ' "=" | string replace --all "=" "─" 
+	set rootdev (df -P / | tail -1 | awk '{print $1}')
+	set curdev  (df -P $PWD | tail -1 | awk '{print $1}')
+	
+	######################
+	# wd
+	if test "$curdev" = "$rootdev"
+		pwd
+	else
+	set -l device (df -P $PWD | tail -1 | awk '{print $1}')
+	set -l label (lsblk -no LABEL $device 2>/dev/null)
+	set -l mountpoint (lsblk -no MOUNTPOINT $device 2>/dev/null)
+	set -l drive_wd (string replace -r "^$mountpoint" "" (pwd))
+
+	echo -e "$CYAN\133$label\135$NC$drive_wd"
+	end
+	
+	######################
+	# seperator
+	set -l amtc (tput cols)
+	set -l sep (string repeat -n $amtc "─")
+	echo -e "$GRAY$sep$NC"
 	
 	######################
 	# ls
