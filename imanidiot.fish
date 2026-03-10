d39523e55ec6afd11f8ce91cccab4afaeb2de474
Chris Jansen <chris9898@live.nl>
2026-03-10 22:33:38 +0100

    'master: idiot function is now colorful idiot function'

--- imanidiot.fish
+++ imanidiot.fish
@@ -1,4 +1,16 @@
 # I'm an idiot.
 function imanidiot
-	echo "im an idiot"
+	set -l text "im an idiot"
+	
+	for char in (string split '' $text)
+		set -l r (random 0 255)
+		set -l g (random 0 255)
+		set -l b (random 0 255)
+		
+		echo -en "\033[38;2;$r;$g;$b"
+		echo -en "m" # gets their own since otherwise fish things its variable $bm above (which doesn't exist).
+		echo -en "$char"
+	end
+	
+	echo ""
 end
